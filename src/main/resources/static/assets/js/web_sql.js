b40526bfb119aad1593ef52ea3a86081b6705acc
Verjun Ripalda <verjun.o.ripalda@accenture.com>
2021-11-18 19:07:32 +0800

    [FE] Integration updates

--- src/main/resources/static/assets/js/web_sql.js
+++ src/main/resources/static/assets/js/web_sql.js
@@ -6,13 +6,41 @@
         init: function() {
             $('#multiselect').multiselect();
 
-            this.loadDBTables();
+            this.onLoadDBTables();
             this.eventHandler();
         },
         eventHandler: function(){
             $("#ci_manual_query_reset").on("click",this.manualQueryReset);
+            $("#ci_quick_view_execute").on("click",this.quickViewExecute);
             $("#ci_quick_view_reset").on("click",this.quickViewReset);
             $("#ci_tables_dropdown").change(this.onSelectDBTable);
+        },
+        quickViewExecute: function() {
+            var data = {
+                tableName: $.trim($("#ci_tables_dropdown").val()),
+                condition: $.trim($("#ci_query_condition").val()),
+                selectedColumns: {}
+            }
+
+            var selectedObj = {};
+            $('#multiselect_to :selected').each(function(){
+                selectedObj[$(this).val()] = $(this).text();
+            });
+
+            data.selectedColumns = selectedObj;
+
+            $.ajax({
+                url: "http://localhost:9092/sql/quickview/select",
+                type:"POST",
+                dataType:"json",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(data),
+                success: function(response) {
+                    console.log(response);
+                },
+                beforeSend: function() {
+                }
+            });
         },
         onSelectDBTable: function() {
             var selectedTable = $(this).val();
@@ -24,20 +52,26 @@
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify({ tableName: selectedTable }),
                 success: function(response) {
-                    console.log(response);
+                    if(response.fieldColumnMap != ""){
+                        $.each(response.fieldColumnMap, function(key, value){
+                            var option = $("<option>");
+                            option.val(key);
+                            option.text(value)
+                            $("#multiselect").append(option);
+                        });
+                    }
                 },
                 beforeSend: function() {
                 }
             });
         },
-        loadDBTables:function() {
+        onLoadDBTables:function() {
             $.ajax({
                 url: "http://localhost:9092/ref/quickview/tablemap",
                 type:"POST",
                 dataType:"json",
                 data: {},
                 success: function(response) {
-
                     if(response.tableMap != ""){
                         $.each(response.tableMap, function(key, value){
                             $("#ci_tables_dropdown").append($("<option>").text(key));
